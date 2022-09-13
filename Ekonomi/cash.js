8ea00d04274177e2c7296acf74205fb7018dbdf4
Glitch (boatneck-clumsy-budget) <none>
2022-09-13 16:09:29 +0000

    💗🎄 Checkpoint ./Ekonomi/cash.js:36589653/17

--- Ekonomi/cash.js
+++ Ekonomi/cash.js
@@ -10,7 +10,7 @@ exports.run = async (client, message, args) => {
   var toplam= cüzdan+banka
 message.channel.send(new Discord.MessageEmbed()
                   .setColor("YELLOW")
-                  .setFooter('Kraken Balance Info!')
+                  .setFooter('Venom Balance Info!')
                   .setThumbnail('https://media.discordapp.net/attachments/852987040659931248/871313810441510922/image0.gif')
                   .setAuthor(user.tag, user.avatarURL({dynamic: true}))     
                   .addField(`Venom Cash 💵 `,`__${toplam ? toplam +'':`0`}__`,true))
