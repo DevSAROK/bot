edcf20f2433d62c57c1afae73ecef1be7e0b9a7f
Glitch (boatneck-clumsy-budget) <none>
2022-09-13 19:51:26 +0000

    👏🚞 Checkpoint ./Oyunlar/slots.js:36589653/590

--- Oyunlar/slots.js
+++ Oyunlar/slots.js
@@ -23,7 +23,7 @@ if(miktar > para) return message.channel.send(new Discord.MessageEmbed()
   if(isNaN(miktar) || miktar < 0) return message.channel.send(new Discord.MessageEmbed()
 .setColor("RED")
 .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
-.setDescription(`<a:emoji_50:872142301789564969>The amount you entered is not a valid number !?`))
+.setDescription(`<a:emoji_50:872142301789564969> The amount you entered is not a valid number !?`))
  let kazandin = miktar*3   
   let slots = ["❤️","❤️","❤️",];
   //
@@ -47,7 +47,7 @@ if(miktar > para) return message.channel.send(new Discord.MessageEmbed()
   if (slots[result1] === slots[result2] && slots[result2] === slots[result3] ) {
     var text2 = `**
     ___SLOTS___  
- |<a:slatszela1:1019216116465291428v> <a:slatszela1:1019216116465291428v> <a:slatszela1:1019216116465291428v>|${message.author.username}
+ |<a:slatszela1:1019216116465291428><a:slatszela1:1019216116465291428|<a:slatszela1:1019216116465291428>|${message.author.username}
 |                  |
 |                  |**`;
           message.channel.send(text2).then(msg => {
@@ -65,7 +65,7 @@ if(miktar > para) return message.channel.send(new Discord.MessageEmbed()
   } else {
     var text3 = `**
     ___SLOTS___  
-|<a:slatszela1:1019216116465291428> <a:slatszela1:1019216116465291428><a:slatszela1:1019216116465291428>|${message.author.username}
+|<a:slatszela1:1019216116465291428><a:slatszela1:1019216116465291428><a:slatszela1:1019216116465291428>${message.author.username}
 |                  |
 |                  |**`;
           message.channel.send(text3).then(msg => {
