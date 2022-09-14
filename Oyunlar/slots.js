c4cc760f5c153d76243cee2d9735590eb5bc491e
Glitch (boatneck-clumsy-budget) <none>
2022-09-14 11:36:59 +0000

    📕🚄 Checkpoint ./Oyunlar/slots.js:36589653/15

--- Oyunlar/slots.js
+++ Oyunlar/slots.js
@@ -47,7 +47,8 @@ if(miktar > para) return message.channel.send(new Discord.MessageEmbed()
   if (slots[result1] === slots[result2] && slots[result2] === slots[result3] ) {
     var text2 = `**
     ___SLOTS___  
- |<a:slatszela1:1019216116465291428><a:slatszela1:1019216116465291428><a:slatszela1:1019216116465291428>${message.author.username}
+ |<a:slatszela1:1019216116465291428><a:slatszela1:1019216116465291428><a:slatszela1:1019216116465291428>
+ ${message.author.username}
 |                  |
 |                  |**`;
           message.channel.send(text2).then(msg => {
@@ -65,7 +66,8 @@ if(miktar > para) return message.channel.send(new Discord.MessageEmbed()
   } else {
     var text3 = `**
     ___SLOTS___  
-|<a:slatszela1:1019216116465291428><a:slatszela1:1019216116465291428><a:slatszela1:1019216116465291428>${message.author.username}
+|<a:slatszela1:1019216116465291428><a:slatszela1:1019216116465291428><a:slatszela1:1019216116465291428>
+${message.author.username}
 |                  |
 |                  |**`;
           message.channel.send(text3).then(msg => {
@@ -84,7 +86,7 @@ if(miktar > para) return message.channel.send(new Discord.MessageEmbed()
 exports.conf = {
   enabled: true,
   guildOnly: true,
-  aliases: ["slot","a","slotmachine"],
+  aliases: ["slot","s","slotmachine"],
   permLevel: 2
 };
 
